077885e86268e2ddb00e1256dc954bd31bb2e7e3
mohammadrezajavid <javid.9@yahoo.com>
2023-07-08 17:19:12 +0330

    update functions

--- httpServer/httpserver.go
+++ httpServer/httpserver.go
@@ -1,11 +1,9 @@
 package httpServer
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"text/template"
-	"time"
 )
 
 type Page struct {
@@ -17,12 +15,29 @@ const SuffixFile = ".txt"
 const PrefixFile = "./txt/"
 const PermissinFile = 0600
 
-func (page *Page) save() error {
+var templates = template.Must(template.ParseFiles("./html/edit.html", "./html/view.html"))
+
+func (page *Page) savePage() error {
 	fileName := PrefixFile + page.Title + SuffixFile
 	return os.WriteFile(fileName, page.Body, PermissinFile)
 }
 
-func loadPage(title string) (*Page, error) {
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+
+	err := page.savePage()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func viewPage(title string) (*Page, error) {
 	fileName := PrefixFile + title + SuffixFile
 	body, err := os.ReadFile(fileName)
 
@@ -33,57 +48,31 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func SaveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
-	body := r.FormValue("body")
-
-	page := &Page{Title: title, Body: []byte(body)}
-	err := page.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func EditHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	page, err := loadPage(title)
-
-	if err != nil {
-		page = &Page{Title: title}
-	}
-	renderTemplate(w, page, "./html/edit.html")
-}
-
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	page, err := loadPage(title)
+	page, err := viewPage(title)
 
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	renderTemplate(w, page, "./html/view.html")
+	renderTemplate(w, "view.html", page)
 }
 
-func renderTemplate(w http.ResponseWriter, page *Page, templateFileName string) {
-	temp, err := template.ParseFiles(templateFileName)
+func EditHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	page, err := viewPage(title)
+
+	if err != nil {
+		page = &Page{Title: title}
+		page.savePage()
+	}
+	renderTemplate(w, "edit.html", page)
+}
+
+func renderTemplate(w http.ResponseWriter, htmlTemplatePath string, page *Page) {
+	err := templates.ExecuteTemplate(w, htmlTemplatePath, page)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-	temp.Execute(w, page)
-
-}
-
-// -----------------------------------------------------
-
-func SayHello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello user...\n")
-	fmt.Printf("[%s]; status: %d\n", time.Now(), 200)
-}
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hi there, I love %s!", r.URL.Path[1:])
 }
--- main.go
+++ main.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/sayhello", httpServer.SayHello)
-	http.HandleFunc("/", httpServer.Handler)
-
 	http.HandleFunc("/view/", httpServer.ViewHandler)
 	http.HandleFunc("/edit/", httpServer.EditHandler)
 	http.HandleFunc("/save/", httpServer.SaveHandler)
